5af0e28f198be55a97571c63c55a47b340c569c1
ottojolanki <ojolanki@stanford.edu>
2018-03-26 10:26:24 -0700

    typo

--- run_simple_mlp_on_mnist.py
+++ run_simple_mlp_on_mnist.py
@@ -18,11 +18,6 @@ num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 
-# flatten 28*28 images to a 784 vector for each image
-num_pixels = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
-
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
